13c2e7ab9c3438d58d9dd46879f7e6143c20ae4a
Tanvir Rudro <rudro987@gmail.com>
2024-05-18 15:14:24 +0600

    refactored setup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
 
-const app : Application = express();
+const app : Application = express()
 
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
 export default app;
-
-
-
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
 
-
 async function main() {
+
+  try {
     await mongoose.connect(config.database_url as string);
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+    app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`);
+
+    });
+  } catch (error) {
+    console.error(error);
   }
+}
 
-
-app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-});
+main().catch((err) => console.log(err));
